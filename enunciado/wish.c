0c82c7a01ae1bc2d52b3d294c43b0521687c93f5
M-Kleber <71222900+M-Kleber@users.noreply.github.com>
2024-04-14 23:36:05 -0500

    execute_command
    Para analizar y ejecutar comandos, se divide la línea de comandos en argumentos y se determina si el comando es un comando integrado o un programa externo.

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -43,3 +43,41 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+void execute_command(char *cmd) {
+    char *args[MAX_INPUT];
+    char *token;
+    int i = 0;
+
+    token = strtok(cmd, " \n\t");
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " \n\t");
+    }
+    args[i] = NULL;  // Null terminate the list of arguments
+
+    // Built-in commands
+    if (strcmp(args[0], "exit") == 0) {
+        exit(0);
+    } else if (strcmp(args[0], "cd") == 0) {
+        if (i != 2) {
+            fprintf(stderr, "An error has occurred\n");
+        } else if (chdir(args[1]) != 0) {
+            fprintf(stderr, "An error has occurred\n");
+        }
+    } else if (strcmp(args[0], "path") == 0) {
+        // Handle setting path here
+    } else {
+        // External commands
+        pid_t pid = fork();
+        if (pid == 0) {
+            if (execvp(args[0], args) == -1) {
+                fprintf(stderr, "An error has occurred\n");
+                exit(1);
+            }
+        } else if (pid > 0) {
+            wait(NULL);
+        } else {
+            fprintf(stderr, "An error has occurred\n");
+        }
+    }
+}
